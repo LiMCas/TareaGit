442ff4011efdc89dfadb97b28971a30ab4b8320b
Limber <42622795+LiMCas@users.noreply.github.com>
2026-03-25 22:07:12 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from datapreputils import delete_null_values,replace_missing_values, min_max, z_score,mode, harmonic_mean, geometric_mean
+
+data = [10,None, 20, 20, 30, 40, None]
+
+print("Datos:", data)
+
+#Limpieza
+print("\n--- Limpieza de datos ---")
+
+df = pd.DataFrame(data)
+data_clean=replace_missing_values(df, method="mean")
+data_clean = delete_null_values(data_clean)
+
+print("\nDataFrame original:")
+print(df)
+print("\nReemplazar valores faltantes:")
+print(data_clean)
+print("\nEliminar valores nulos:")
+print(data_clean)
+
+#Estadisticas
+
+# Convertir a lista
+new_data_list = data_clean[0].tolist()
+
+print("\n--- Estadísticas ---")
+print("Moda:", mode(new_data_list))
+print("Media armónica:", harmonic_mean(new_data_list))
+print("Media geométrica:", geometric_mean(new_data_list))
+
+#Normalizacion
+print("\n--- Normalización ---")
+print("Min-Max:", min_max(new_data_list))
+print("Z-score:", z_score(new_data_list))
