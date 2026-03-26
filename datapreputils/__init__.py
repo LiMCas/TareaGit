935f1821b005a747a30d72a4801970ee58b90ebd
inez2021 <clara_ines@live.com>
2026-03-25 22:44:06 -0400

    Commit tarea Clara

--- datapreputils/__init__.py
+++ datapreputils/__init__.py
@@ -1,3 +1,4 @@
 from .cleaning import delete_null_values,replace_missing_values
 from .normalization import min_max, z_score
 from .statistics import mode, harmonic_mean, geometric_mean
+from .modulo_Clara import harmonic_mean
